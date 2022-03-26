859ddff95bd512a89d610e94abbce70f47c28c43
ymgyt <yamaguchi7073xtt@gmail.com>
2022-03-26 16:11:48 +0900

    chore: add criterion

---
+++ calculator/benches/my_benchmark.rs
@@ -0,0 +1,11 @@
+use calculator::Calculator;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("parse_line 20", |b| {
+        b.iter(|| Calculator::new().calculate_line(black_box("(10 + 20) * 30 / 5")))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
