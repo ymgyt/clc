c7ec41cd135399d075a357d368343b40f61ce81d
ymgyt <yamaguchi7073xtt@gmail.com>
2022-03-29 20:15:38 +0900

    doc: update README (#28)

--- clc-cli/src/main.rs
+++ clc-cli/src/main.rs
@@ -1,17 +1,7 @@
 #![warn(missing_docs)]
 
 //! Clc is a Command line calculator written in Rust 🦀
-//!
-//! # Usage
-//! ```text
-//! $ clc
-//! Version: v0.1.2
-//! To quit, press Ctrl+C or type quit
-//! ❯ sqrt(sqrt(16)) * (100 - 1) * (100 + 1) / 9
-//! 2222
-//! ❯ quit
-//! bye
-//! ```
+//! It eval given expression and print result.
 
 mod cli;
 mod prompt;
